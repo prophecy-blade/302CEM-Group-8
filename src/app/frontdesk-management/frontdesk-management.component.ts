1bea93124db706e60b6f823bae0f203406f8f69a
Nicole Wong Yung Cheng <34061869+prophecy-blade@users.noreply.github.com>
2018-11-22 18:58:51 +0800

    Add files via upload
    Created frontdesk management

---
+++ src/app/frontdesk-management/frontdesk-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frontdesk-management',
+  templateUrl: './frontdesk-management.component.html',
+  styleUrls: ['./frontdesk-management.component.css']
+})
+export class FrontdeskManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
